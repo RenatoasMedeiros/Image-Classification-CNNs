d379e49161f1e7f041f3c9858fb5b858e14fa55d
renato <renatomedeiros2001@gmail.com>
2024-06-01 17:31:51 +0100

    improving the data augmentation to seek better results, and also adding some dropouts

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ test_dir = './dataset/test'
 # Create image data generators
 train_datagen = ImageDataGenerator(
     rescale=1./255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
+    rotation_range=40,
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    shear_range=0.3,
+    zoom_range=0.3,
     horizontal_flip=True,
     fill_mode='nearest')
 
@@ -66,12 +66,15 @@ test_generator = test_datagen.flow_from_directory(
 model = Sequential([
     Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
     MaxPooling2D((2, 2)),
+    Dropout(0.3),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D((2, 2)),
+    Dropout(0.3),
     Conv2D(128, (3, 3), activation='relu'),
     MaxPooling2D((2, 2)),
     Flatten(),
     Dense(512, activation='relu'),
+    Dropout(0.5),
     Dense(NUM_CLASSES, activation='softmax')
 ])
 
